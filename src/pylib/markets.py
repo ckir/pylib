753d3a89b2606d747fb99c777df82411aae97852
ckir <ckir1508@hotmail.com>
2024-01-26 10:30:06 +0200

    Initial Commit

---
+++ src/pylib/markets.py
@@ -0,0 +1,11 @@
+import os
+import logging
+from pylib import __version__
+
+__author__ = "ckir"
+__copyright__ = "ckir"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+import redis
